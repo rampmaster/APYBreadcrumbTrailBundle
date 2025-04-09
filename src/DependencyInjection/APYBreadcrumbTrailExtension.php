1b739fff70d20fd7affb91cda52230afbb786faa
Roberto Moreno <rampmaster@gmail.com>
2025-04-09 06:52:24 -0400

    fix: configuration

--- src/DependencyInjection/APYBreadcrumbTrailExtension.php
+++ src/DependencyInjection/APYBreadcrumbTrailExtension.php
@@ -15,35 +15,19 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\HttpKernel\Kernel;
 
 class APYBreadcrumbTrailExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
+        $loader = new XmlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $loader->load('services.xml');
+
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('apy_breadcrumb_trail.template', $config['template']);
+        $definition = $container->getDefinition('apy_breadcrumb_trail.template');
+        $definition->replaceArgument(0, $config['template']);
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-
-        $this->deprecateService($container, 'apy_breadcrumb_trail');
-        $this->deprecateService($container, 'apy_breadcrumb_trail.annotation.listener');
-    }
-
-    private function deprecateService(ContainerBuilder $container, string $id)
-    {
-        $alias = $container->getAlias($id);
-        if (version_compare(Kernel::VERSION, '5.1.0', '>=')) {
-            $alias->setDeprecated('APY/BreadcrumbTrailBundle', '1.7', 'The service is deprecated, use "%alias_id%" FQCN as service id instead.');
-        } elseif (version_compare(Kernel::VERSION, '4.3.0', '>=')) {
-            /* @phpstan-ignore-next-line */
-            $alias->setDeprecated();
-        }
     }
 }
